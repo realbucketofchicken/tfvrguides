299aacc5ce88388aa34fb053ca1592128b8742e1
bucket <abucketofchicken@proton.me>
2026-01-11 20:23:26 +0100

    Update Nope.js

--- Nope.js
+++ Nope.js
@@ -1,6 +1,7 @@
 function Nope(){
     var audio = new Audio('Syrsa - RMX - Surprise.ogg')
     audio.play()
+    
     var x = document.getElementById("spyscare");
     if (x.style.display === "none"){
         x.style.display = "block";
