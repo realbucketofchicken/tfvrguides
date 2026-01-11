85e443402e1298e37a004731b74cc96c1fe2b3b3
bucket <abucketofchicken@proton.me>
2026-01-11 19:59:53 +0100

    im just messing around atp

--- Nope.js
+++ Nope.js
@@ -1,4 +1,8 @@
 function Nope(){
     var audio = new Audio('nope-sfx.mp3')
     audio.play()
+    var x = document.getElementById("spyscare");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    }
 }
