71f11282e388b32680c0912ac95b15bc7c9eeb0d
bucket <abucketofchicken@proton.me>
2026-01-11 20:25:32 +0100

    dosent like javascript files ig

--- Nope.js
+++
@@ -1,9 +0,0 @@
-function Nope(){
-    var audio = new Audio('Syrsa - RMX - Surprise.ogg')
-    audio.play()
-    
-    var x = document.getElementById("spyscare");
-    if (x.style.display === "none"){
-        x.style.display = "block";
-    }
-}
